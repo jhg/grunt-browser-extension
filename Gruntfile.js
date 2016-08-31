3dc4e613effc11ca786403a92c71419a612940d7
Jesús Hernández Gormaz <jesus.hernandez@tuguu.com>
2016-08-31 16:16:10 +0100

    Add time-grunt for develop

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,8 @@
 
 
 module.exports = function(grunt) {
+    // measures the time each task takes
+    require('time-grunt')(grunt);
     // Project configuration.
     grunt.initConfig({
         eslint: {
