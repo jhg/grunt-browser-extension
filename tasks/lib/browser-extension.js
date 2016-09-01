2ebfa4e81066bb5ab731840983a01cd43058aa38
Jesús Hernández Gormaz <jesus.hernandez@tuguu.com>
2016-09-01 11:20:14 +0100

    Better browser object in context for render

--- tasks/lib/browser-extension.js
+++ tasks/lib/browser-extension.js
@@ -98,7 +98,9 @@ browserExtension.prototype.copyBrowserFiles = function() {
                 pre_processor = browserProcessors[browser];
             }
             var context = pre_processor(JSON.parse(JSON.stringify(options)));
-            context.browser = {};
+            context.browser = {
+                name: browser
+            };
             context.browser[browser] = true;
             // Render template with a context and write to file
             grunt.file.write(path.join(
@@ -132,7 +134,9 @@ browserExtension.prototype.copyUserFiles = function() {
             if (isTemplate) {
                 var template = handlebars.compile(grunt.file.read(abspath));
                 var context = JSON.parse(JSON.stringify(self.options));
-                context.browser = {};
+                context.browser = {
+                    name: browser
+                };
                 context.browser[browser] = true;
                 var raw = template(context);
                 grunt.file.write(dstpath, raw);
