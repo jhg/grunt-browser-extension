ee751fae38f321f5edc474a97e37ed6a87b2ec33
Jesús Hernández Gormaz <jesus.hernandez@tuguu.com>
2016-09-06 10:54:31 +0100

    Small refactor and update of versions of dependencies

--- tasks/browser_extension.js
+++ tasks/browser_extension.js
@@ -16,39 +16,44 @@ module.exports = function (grunt) {
     var BrowserExtension = require('./lib/browser-extension')(grunt);
 
     grunt.registerMultiTask('browser_extension', 'Grunt plugin to create any browser website extension', function () {
-        if(this.target){
-            var options = this.options();
-            var required_options = [
-                'id',
-                'name',
-                'author',
-                'description',
-                'host',
-                'version'
-            ];
-            for(var required_options_id in required_options){
-                if(required_options_id){
-                    var required_option = required_options[required_options_id];
-                    if(!util.isString(options[required_option])){
-                        grunt.fail.fatal("Please set up all required options. All options must be string value! You have not setted " + required_option);
-                    }
+        // Some pre-checks
+        if(!this.target){
+            grunt.fail.fatal('This task need a target!');
+            return;
+        }
+        var options = this.options();
+        var required_options = [
+            'id',
+            'name',
+            'author',
+            'description',
+            'host',
+            'version'
+        ];
+        for(var required_options_id in required_options){
+            if(required_options_id){
+                var required_option = required_options[required_options_id];
+                if(!util.isString(options[required_option])){
+                    grunt.fail.fatal("Please set up all required options. All options must be string value! You have not setted " + required_option);
                 }
             }
-            var pluginRoot = path.join(path.dirname(fs.realpathSync(__filename)), '../');
-            var browserExt = new BrowserExtension(pluginRoot, options, this.target, grunt);
-            grunt.verbose.ok('Start build extension steps');
-
-            browserExt.copyUserFiles();
-            grunt.verbose.ok('User files copied');
-
-            browserExt.copyBrowserFiles();
-            grunt.verbose.ok('Extension files copied');
-
-            browserExt.buildNsisIE();
-            grunt.verbose.ok('NSIS installer for IE builded');
-
-            browserExt.build();
-            grunt.verbose.ok('Extensions builded');
         }
+
+        // Steps for build
+        var pluginRoot = path.join(path.dirname(fs.realpathSync(__filename)), '../');
+        var browserExt = new BrowserExtension(pluginRoot, options, this.target, grunt);
+        grunt.verbose.ok('Start build extension steps');
+
+        browserExt.copyUserFiles();
+        grunt.verbose.ok('User files copied');
+
+        browserExt.copyBrowserFiles();
+        grunt.verbose.ok('Extension files copied');
+
+        browserExt.buildNsisIE();
+        grunt.verbose.ok('NSIS installer for IE builded');
+
+        browserExt.build();
+        grunt.verbose.ok('Extensions builded');
     });
 };
